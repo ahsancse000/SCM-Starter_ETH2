11454748938809353acb9c94c0be618e4734887d
ahsancse000 <165180664+ahsancse000@users.noreply.github.com>
2024-07-02 23:03:22 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,24 @@
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import SolidFundrABI from "../artifacts/contracts/SolidFundr.sol/SolidFundr.json";
+import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+
 
 export default function HomePage() {
   const [ethWallet, setEthWallet] = useState(undefined);
   const [account, setAccount] = useState(undefined);
-  const [contract, setContract] = useState(undefined);
+  const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
-  const [funds, setFunds] = useState([]);
-  const [fundIdInput, setFundIdInput] = useState("");
-  const [donationAmountInput, setDonationAmountInput] = useState("");
-  const [contributors, setContributors] = useState([]);
-  const [targetAmountInput, setTargetAmountInput] = useState("");
-  const [targetAddressInput, setTargetAddressInput] = useState("");
-  const [titleInput, setTitleInput] = useState("");
-  const [descriptionInput, setDescriptionInput] = useState("");
+  const [recipient, setRecipient] = useState("");
+  const [amount, setAmount] = useState("");
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-  const contractABI = SolidFundrABI.abi;
+  const atmABI = atm_abi.abi;
 
-  const initWallet = async () => {
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
+
     if (ethWallet) {
       const account = await ethWallet.request({ method: "eth_accounts" });
       handleAccount(account);
@@ -47,178 +43,150 @@ export default function HomePage() {
     const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
     handleAccount(accounts);
 
+    // once wallet is set we can get a reference to our deployed contract
+    getATMContract();
+  };
+
+  const getATMContract = () => {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
-    const contractInstance = new ethers.Contract(contractAddress, contractABI, signer);
-    setContract(contractInstance);
+    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
 
-    updateBalance();
+    setATM(atmContract);
   };
 
-  const updateBalance = async () => {
-    if (contract && account) {
-      const userBalance = await contract.getContributions(account);
-      setBalance(userBalance);
+  const getBalance = async () => {
+    if (atm) {
+      setBalance((await atm.getBalance()).toNumber());
     }
   };
 
-  const donateToFund = async () => {
-    const fundId = parseInt(fundIdInput);
-    const amount = ethers.utils.parseEther(donationAmountInput);
-
-    try {
-      const donationTx = await contract.donate(fundId, { value: amount });
-      await donationTx.wait();
-      updateBalance();
-      alert("Donation successful!");
-    } catch (error) {
-      console.error("Error donating to fund:", error);
-      alert("Failed to donate to fund. Please try again.");
+  const deposit = async () => {
+    if (atm) {
+      let tx = await atm.deposit(1);
+      await tx.wait();
+      getBalance();
     }
   };
 
-  const createFund = async () => {
-    const targetAmount = ethers.utils.parseEther(targetAmountInput);
-    const targetAddress = targetAddressInput;
-    const title = titleInput;
-    const description = descriptionInput;
-
-    try {
-      const createFundTx = await contract.createFund(targetAmount, targetAddress, title, description);
-      await createFundTx.wait();
-      alert("Campaign created successfully!");
-    } catch (error) {
-      console.error("Error creating campaign:", error);
-      alert("Failed to create campaign. Please try again.");
+  const withdraw = async () => {
+    if (atm) {
+      let tx = await atm.withdraw(1);
+      await tx.wait();
+      getBalance();
+    }
+  };
+  const send = async () => {
+    if (atm) {
+      //if (atm && recipient && amount) {
+      let tx = await atm.send(recipient, amount);
+      await tx.wait();
+      getBalance();
     }
   };
 
-  const fetchFunds = async () => {
-    try {
-      const funds = await contract.getFunds();
-      setFunds(funds);
-    } catch (error) {
-      console.error("Error fetching funds:", error);
-      alert("Failed to fetch funds. Please try again.");
+  const initUser = () => {
+    // Check to see if user has Metamask
+    if (!ethWallet) {
+      return <p>Please install Metamask in order to use this ATM.</p>;
     }
-  };
 
-  const fetchContributors = async () => {
-    try {
-      const fundId = parseInt(fundIdInput);
-      const fundContributors = await contract.getDonations(fundId);
-      setContributors(fundContributors);
-    } catch (error) {
-      console.error("Error fetching contributors:", error);
-      alert("Failed to fetch contributors. Please try again.");
+    // Check to see if user is connected. If not, connect to their account
+    if (!account) {
+      return (
+        <button onClick={connectAccount}>
+          Please connect your Metamask wallet
+        </button>
+      );
     }
+
+    if (balance == undefined) {
+      getBalance();
+    }
+
+    return (
+      <div>
+        <p>Your Account: {account}</p>
+        <p>Your Balance: {balance}</p>
+        <div className="cus">
+          <button onClick={deposit}>Deposit 1 ETH</button>
+          <button onClick={withdraw}>Withdraw 1 ETH</button>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Recipient Address"
+            value={recipient}
+            onChange={(e) => setRecipient(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <button onClick={send} className="custom-button">
+            Send
+          </button>
+        </div>
+      </div>
+    );
   };
 
   useEffect(() => {
-    initWallet();
+    getWallet();
   }, []);
 
   return (
     <main className="container">
       <header>
-        <h1>Welcome to the SolidFundr Platform!</h1>
+        <div className="a">
+          <h1>Metamask Wallet Integration</h1>
+        </div>
       </header>
-      <section>
-        <h2>User Information</h2>
-        <p>Account: {account}</p>
-        <p>Balance: {balance}</p>
-      </section>
-      <section>
-        <h2>Actions</h2>
-        <button onClick={connectAccount}>Connect Wallet</button>
-      </section>
-      <section>
-        <h2>Donate to Fund</h2>
-        <div>
-          <label htmlFor="fundIdInput">Fund ID:</label>
-          <input
-            type="text"
-            id="fundIdInput"
-            value={fundIdInput}
-            onChange={(e) => setFundIdInput(e.target.value)}
-          />
-          <label htmlFor="donationAmountInput">Donation Amount (ETH):</label>
-          <input
-            type="text"
-            id="donationAmountInput"
-            value={donationAmountInput}
-            onChange={(e) => setDonationAmountInput(e.target.value)}
-          />
-          <button onClick={donateToFund}>Donate</button>
+      <div>
+        <div style={{ textAlign: "center", fontSize: "30px" }}>
+         
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginBottom: "35px",
+            }}
+          >
+           
+          </div>
         </div>
-      </section>
-      <section>
-        <h2>Create Fund</h2>
-        <div>
-          <label htmlFor="targetAmountInput">Target Amount (ETH):</label>
-          <input
-            type="text"
-            id="targetAmountInput"
-            value={targetAmountInput}
-            onChange={(e) => setTargetAmountInput(e.target.value)}
-          />
-          <label htmlFor="targetAddressInput">Target Address:</label>
-          <input
-            type="text"
-            id="targetAddressInput"
-            value={targetAddressInput}
-            onChange={(e) => setTargetAddressInput(e.target.value)}
-          />
-          <label htmlFor="titleInput">Title:</label>
-          <input
-            type="text"
-            id="titleInput"
-            value={titleInput}
-            onChange={(e) => setTitleInput(e.target.value)}
-          />
-          <label htmlFor="descriptionInput">Description:</label>
-          <input
-            type="text"
-            id="descriptionInput"
-            value={descriptionInput}
-            onChange={(e) => setDescriptionInput(e.target.value)}
-          />
-          <button onClick={createFund}>Create Fund</button>
-        </div>
-      </section>
-      <section>
-        <h2>Funds</h2>
-        <button onClick={fetchFunds}>Fetch Funds</button>
-        <ul>
-          {funds.map((fund, index) => (
-            <li key={index}>
-              <p>Title: {fund.title}</p>
-              <p>Description: {fund.description}</p>
-              <p>Amount: {ethers.utils.formatEther(fund.amount)} ETH</p>
-              <p>Target Amount: {ethers.utils.formatEther(fund.targetAmount)} ETH</p>
-              <p>Completed: {fund.completed.toString()}</p>
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section>
-        <h2>Contributors</h2>
-        <button onClick={fetchContributors}>Fetch Contributors</button>
-        <ul>
-          {contributors.map((contributor, index) => (
-            <li key={index}>
-              <p>Address: {contributor.author}</p>
-              <p>Amount: {ethers.utils.formatEther(contributor.amount)} ETH</p>
-            </li>
-          ))}
-        </ul>
-      </section>
+      </div>
+      {initUser()}
       <style jsx>{`
+        body {
+          background-color: black;
+        }
         .container {
           text-align: center;
+          background-color: black;
+          color: white;
         }
-      `}
-      </style>
+        .a {
+          background-color: black;
+          padding: 10px;
+          margin: 20px;
+        }
+        .cus {
+          background-color: #b48241;
+          color: white;
+          border: none;
+          margin-right: 50px;
+          border-radius: 50px;
+          cursor: pointer;
+          transition: transform 0.3s ease-in-out;
+        }
+
+        .custom-button:hover {
+          transform: translateY(-5px);
+        }
+      `}</style>
     </main>
   );
 }

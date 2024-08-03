b44866b139640e9fc5a78ffae440aca1dafbcdb2
ahsancse000 <165180664+ahsancse000@users.noreply.github.com>
2024-08-03 22:47:02 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,140 +1,75 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract SolidFundr {
-    // Fund structure
-    struct Fund {
-        uint256 id;
-        address creator;
-        uint creationDate;
-        uint256 amount;
-        uint256 targetAmount;
-        address targetAddress;
-        string title;
-        string description;
-        bool completed;
-    }
-    // Donation structure
-    struct Donation {
-        uint256 amount;
-        address author;
-    }
-    // List of funds
-    Fund[] public listFunds;
-    // Counter to keep track of funds
-    uint public fundCounter;
-    // List of donations for each fund
-    mapping(uint256 => Donation[]) public contributionFunds;
-    // List of contributions for each author
-    mapping(address => uint256) public contributionAuthors;
+//import "hardhat/console.sol";
 
-    // Events
-    event FundCreated(uint256 id);
-    event DonationCreated(uint256 amount, address author);
-    event FundCompleted(uint256 id);
+contract Assessment {
+    address payable public owner;
+    uint256 public balance;
 
-    // Custom errors
-    error FundDoesNotExist();
-    error AmountLessThanZero();
-    error AddressNotValid();
-    error FundAlreadyCompleted();
-    error SendAmountFailed();
+    event Deposit(uint256 amount);
+    event Withdraw(uint256 amount);
 
-    // Function to create a new Fund
-    function createFund(
-        uint256 targetAmount,
-        address targetAddress,
-        string calldata title,
-        string calldata description
-    ) public {
-        if (targetAmount <= 0) revert AmountLessThanZero();
-        if (targetAddress == address(0)) revert AddressNotValid();
-
-        // Create new fund
-        Fund memory newFund = Fund(
-            fundCounter,
-            msg.sender,
-            block.timestamp,
-            0,
-            targetAmount,
-            targetAddress,
-            title,
-            description,
-            false
-        );
-        listFunds.push(newFund);
-
-        emit FundCreated(fundCounter);
-        fundCounter++;
+    constructor(uint initBalance) payable {
+        owner = payable(msg.sender);
+        balance = initBalance;
     }
 
-    // Function to create a donation to a fund
-    function donate(uint256 fundId) public payable {
-        uint256 amount = msg.value;
+    function getBalance() public view returns(uint256){
+        return balance;
+    }
 
-        // Checks
-        if (amount <= 0) revert AmountLessThanZero();
-        if (fundId >= fundCounter) revert FundDoesNotExist();
-        if (listFunds[fundId].completed == true) revert FundAlreadyCompleted();
+    function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
 
-        // Update amount of contribution for author
-        contributionAuthors[msg.sender] =
-            contributionAuthors[msg.sender] +
-            amount;
+        // make sure this is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
 
-        // Update amount of fund
-        listFunds[fundId].amount = listFunds[fundId].amount + amount;
+        // perform transaction
+        balance += _amount;
 
-        // Check if Fund is completed
-        if (listFunds[fundId].amount >= listFunds[fundId].targetAmount) {
-            sendFund(fundId);
+        // assert transaction completed successfully
+        assert(balance == _previousBalance + _amount);
+
+        // emit the event
+        emit Deposit(_amount);
+    }
+
+    // custom error
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    function withdraw(uint256 _withdrawAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint _previousBalance = balance;
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
         }
 
-        // Create new donation
-        Donation memory donation = Donation(amount, msg.sender);
-        contributionFunds[fundId].push(donation);
+        // withdraw the given amount
+        balance -= _withdrawAmount;
 
-        emit DonationCreated(amount, msg.sender);
+        // assert the balance is correct
+        assert(balance == (_previousBalance - _withdrawAmount));
+
+        // emit the event
+        emit Withdraw(_withdrawAmount);
     }
 
-    // Internal function to send amount of fund if it is completed
-    function sendFund(uint256 fundId) internal {
-        if (fundId >= fundCounter) revert FundDoesNotExist();
-        if (listFunds[fundId].completed == true) revert FundAlreadyCompleted();
-
-        // Send amount of fund to the target address
-        (bool result, ) = payable(listFunds[fundId].targetAddress).call{
-            value: listFunds[fundId].amount
-        }("");
-        if (result == false) revert SendAmountFailed();
-
-        // Set completed the fund
-        listFunds[fundId].completed = true;
-
-        emit FundCompleted(fundId);
+     function transfer(address payable _to) public payable {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(balance >= 1 ether, "Insufficient balance to transfer 1 ETH");
+        
+        // Transfer 1 ETH
+        _to.transfer(1 ether);
+        
+        // Update balances
+        balance -= 1 ether;
+        
+        // Emit transfer event
+        emit Transfer(_to, 1 ether);
     }
 
-    // Function to return list of funds
-    function getFunds() public view returns (Fund[] memory) {
-        return listFunds;
-    }
-
-    // Function to return list of donations of a fund
-    function getDonations(
-        uint256 fundId
-    ) public view returns (Donation[] memory) {
-        if (fundId >= fundCounter) revert FundDoesNotExist();
-        return contributionFunds[fundId];
-    }
-
-    // Function to return total amount of contributions of an author
-    function getContributions(address author) public view returns (uint256) {
-        return contributionAuthors[author];
-    }
-
-    // Function to receive Ether. msg.data must be empty
-    receive() external payable {}
-
-    // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
 }
